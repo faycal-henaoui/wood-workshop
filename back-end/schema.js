faf0eb2f80bf6d6ff8a9eceee110a18856f15cef
faycal-henaoui <henaoui.faycal@univ-tlemcen.dz>
2026-01-31 15:25:24 +0100

    Update schema script to use DATABASE_URL

--- back-end/schema.js
+++ back-end/schema.js
@@ -5,12 +5,14 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
+// Use same logic as db.js
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction ? { rejectUnauthorized: false } : false
 });
 
 const sql = `
